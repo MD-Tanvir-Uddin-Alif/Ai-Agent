f0b52938023089d4be881da621ec4c39a94fa109
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-07-23 00:11:06 +0600

    pdf loader tool created

---
+++ Ai_research_assistent/tools/pdf_loader.py
@@ -0,0 +1,18 @@
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_and_chunk_pdf(file_path, chunk_size=800, chunk_overlap=150):
+    try:
+        loader = PyMuPDFLoader(file_path)
+        docs = loader.load()
+        
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size = chunk_size,
+            chunk_overlap = chunk_overlap
+        )
+        
+        chunks = splitter.split_documents(docs)
+        return chunks
+    except Exception as e:
+        print(f"[ERROR] Failed to load and chunk PDF: {e}")
+        return[]
